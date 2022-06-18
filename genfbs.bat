7eac59a8ed19ac6f214529b9d9283637cc4e46b8
Kent <642238553@qq.com>
2022-06-18 18:25:03 +0800

    init fbx_plugin

---
+++ genfbs.bat
@@ -0,0 +1,2 @@
+DEL fbs_generated/*
+FOR %%i IN (fbs\*.*) DO flatc --cpp -o fbs_generated/ --filename-suffix "" %%i
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Admin on 2022/6/18.
+//
+#include "scene.h"
+
+int main() {
+    return 0;
+}
